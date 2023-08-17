645c086850156d909f13961a2ff753d77798f958
Robbert Wijtman <robbert@xinxmedia.com>
2023-08-16 17:29:59 -0700

    vite always full reload

--- project_files/app_vite.config.js
+++ project_files/app_vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const fullReloadAlways = {
+  handleHotUpdate({ server }) {
+    server.ws.send({ type: "full-reload" })
+    return []
+  },
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), fullReloadAlways],
   server: {
     port: 8080,
   },
