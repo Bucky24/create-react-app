69aee9ae443b7b3f6e6411710bcc7a8b9cdfbf05
Robbert Wijtman <robbert@xinxmedia.com>
2023-11-28 17:40:04 -0800

    better webpack version

--- index.js
+++ index.js
@@ -377,7 +377,7 @@ async function createReactLib() {
 			"file-loader": "6.2.0",
 			"html-webpack-plugin": "5.2.0",
 			"style-loader": "2.0.0",
-			"webpack": "5.74.0",
+			"webpack": "5.89.0",
 			"webpack-cli": "4.10.0",
 			"webpack-dev-server": "4.7.4",
 			"react-dom": "18.2.0",
